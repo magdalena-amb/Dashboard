34436bd997eb9a2f7280d7bae07df45fda006e29
Magdalena Ambrozy <magda.ambrozy@gmail.com>
2019-05-21 12:34:17 +0200

    Add and modify files

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var sidebarToggler= document.getElementById('sidebar-toggler');
+  var sidebar = document.getElementById('sidebar');
+  function toggle(){
+    sidebar.classList.toggle('hide-show');
+    sidebar.classList.toggle('display');
+  }
+  sidebarToggler.addEventListener('click', toggle);
+
+var dateControl = document.querySelectorAll('input[type="date"]');
+  Array.from(dateControl).forEach(function(element){ 
+  element.value = '2019-05-14'
+
+  //Wyświetlanie dzisiejszej daty domyślnie w input[type='date'] //
+  //jaki jest sposób, bo tutaj :
+  //error: ten format nie jest obsługiwany //
+  //   var today = function(){
+  //     var Today = new Date();
+  //     var Month = Today.getMonth();
+  //     var Day = Today.getDate();
+  //     var Year = Today.getFullYear();
+  //        if(Year <= 99)    Year += 1900
+  //     return `${Day}.${Month}.${Year}`;
+  //   }
+  // element.value = today();
+    });
